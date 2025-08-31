adce3fb5dfd3dc4df10c2ad9c761d34a89c8c7ca
casqueen <casterpain@gmail.com>
2025-08-30 23:30:32 -0500

    updated the App.jsx and index.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./components/Dashboard";
 import Register from "./components/Register";
 import StudentList from "./pages/StudentList";
@@ -15,34 +14,38 @@ import TakeExam from "./pages/TakeExam";
 import Result from "./pages/Result";
 import ExamResults from "./pages/ExamResults";
 import StudentExamList from "./pages/StudentExamList";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100"> 
-        <NavbarComponent />
-        <main className="flex-grow-1"> 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/students" element={<StudentList />} />
-            <Route path="/exams" element={<ExamList />} />
-            <Route path="/exams/view/:id" element={<ExamDetails />} />
-            <Route path="/exams/create" element={<CreateExam />} />
-            <Route path="/exams/edit/:id" element={<CreateExam />} />
-            <Route path="/exams/take/:id" element={<TakeExam />} />
-            <Route path="/result/:id" element={<Result />} />
-            <Route path="/exams/results/:examId" element={<ExamResults />} />
-            <Route path="/exam-list" element={<StudentExamList />} />
-          </Routes>
-        </main>
-        <Footer /> 
-        <ToastContainer position="top-right" autoClose={3000} />
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <NavbarComponent />
+          <main style={{ flexGrow: 1 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/students" element={<StudentList />} />
+              <Route path="/exams" element={<ExamList />} />
+              <Route path="/exams/view/:id" element={<ExamDetails />} />
+              <Route path="/exams/create" element={<CreateExam />} />
+              <Route path="/exams/edit/:id" element={<CreateExam />} />
+              <Route path="/exams/take/:id" element={<TakeExam />} />
+              <Route path="/result/:id" element={<Result />} />
+              <Route path="/exams/results/:examId" element={<ExamResults />} />
+              <Route path="/exam-list" element={<StudentExamList />} />
+            </Routes>
+          </main>
+          <Footer />
+          <ToastContainer position="top-right" autoClose={3000} />
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
